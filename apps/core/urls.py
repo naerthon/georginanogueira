68092fd04d0e500a0c6c4851c3bc8ceda3ef461a
naerthon <naerthon@gmail.com>
2018-01-11 12:36:15 -0200

    Update nav

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -3,7 +3,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 from .views import index, comocomprar, galeria, contato, eventos,detail_eventos,detail_colecoes,detail_galeria
 
-
 urlpatterns = [
 	url(r'^$', index, name='index'),
 	url(r'^galeria/$', galeria, name='all-galeria'),
